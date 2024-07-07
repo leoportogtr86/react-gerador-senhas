1a1ac0a78c8a29baf5076526f3c3682a4c19e072
Leo Porto <leonardoreisporto@gmail.com>
2024-07-07 19:37:13 -0300

    listas

--- src/components/Lista/index.jsx
+++ src/components/Lista/index.jsx
@@ -7,7 +7,8 @@ const Lista = () => {
     const [lista, setLista] = useState([]);
     const [item, setItem] = useState("");
 
-    const addItem = () => {
+    const addItem = (e) => {
+        e.preventDefault();
         if (item.trim() === "") {
             return;
         }
@@ -17,16 +18,18 @@ const Lista = () => {
     return (
         <div className="container">
             <h1>Criando Listas com React</h1>
-            <label>
-                Adicione itens a sua lista
-                <input
-                    type="text"
-                    placeholder="Incluir um item"
-                    value={item}
-                    onChange={(e) => setItem(e.target.value)}
-                />
-            </label>
-            <button onClick={addItem} type="button">Adicionar</button>
+            <form onSubmit={addItem}>
+                <label>
+                    Adicione itens a sua lista
+                    <input
+                        type="text"
+                        placeholder="Incluir um item"
+                        value={item}
+                        onChange={(e) => setItem(e.target.value)}
+                    />
+                </label>
+                <button onClick={addItem} type="button">Adicionar</button>
+            </form>
             <ul className="lista">
                 {
                     lista.map((item, index) => {
